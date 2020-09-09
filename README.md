b993c832409cc2333560876c79c1fc905cfd7592
muyao <muyao1987@qq.com>
2020-09-09 21:28:33 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,31 @@
+# Mars3D  三维地球平台 功能示例
+
+此仓库代码非平台的全部功能示例，仅开放部分示例用于大家测试和试用。完整示例体验请访问:[http://cesium.marsgis.cn/examples.html](http://cesium.marsgis.cn/examples.html)
+
+如需更多示例进行测试或试用，请联系我们：[微信](http://marsgis.cn/weixin.html )
+
+
+## Mars3D平台介绍
+ Mars3D三维地球平台软件,简称Mars3D（曾用名 MarsGIS for Cesium ） 是[火星科技](http://www.marsgis.cn/)研发的一个Web三维地图开发平台系统，是火星科技团队成员多年GIS开发和Cesium使用的技术沉淀。基于Cesium和现代Web技术栈全新构建， 集成了领先的开源地图库、可视化库，提供了全新的大数据可视化、实时流数据可视化功能，通过本产品可快速实现浏览器和移动端上美观、流畅的地图呈现与空间分析。 框架主要目的是升级Cesium的基础功能和编写相关示例，方便快速搭建Cesium地图项目，敏捷开发，可复用，支持各种配置，适合各种场景使用。
+ 
+ 更多相关，请参考 [Mars3D开源导航](https://github.com/marsgis/MarsGIS-for-Cesium)
+ 
+ 
+
+## 使用说明
+ 在任意开发编辑器（如vscode等）或http服务器(如node、nginx、tomcat、IIS等)下直接运行浏览index.html即可
+
+### 压缩及混淆
+ build整站压缩及混淆：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+
+ 
+
+## 未开源目录列表
+除了下面文件未开源外，其他所有代码均可以阅读。
+1. \index.html 
+2. \cesium-example\editor.html 
+3. \cesium-example\widgets\ *
+ 
+## 版权说明
+  本项目主要是为了展示[Mars3D](http://cesium.marsgis.cn)的项目应用，仅限大家学习之用，如需用于商业项目，请联系购买[火星科技](http://cesium.marsgis.cn)SDK授权。
+ 并且Mars3D-SDK类库并未开源（即`libs/cesiumjs/mars3d/`）,在线演示版内部有作者公司logo及时效限制。
---
+++ cesium-example/nav/config.js
@@ -0,0 +1,352 @@
+/**
+ * Cesium 示例配置文件：包含示例的分类、名称、缩略图、文件路径
+ */
+
+
+/**
+ * key值：为exampleConfig配置的key值或者fileName值 （为中间节点时是key值，叶结点是fileName值）
+ * value值：fontawesome字体icon名 
+ */
+var sideBarIconConfig = {
+    "imageryLayer": "fa-photo",
+    "feature": "fa-road",
+    "gltf": "fa-space-shuttle",
+    "3dtiles": "fa-building-o",
+    "scene": "fa-globe",
+    "analysi": "fa-signal",
+    "hangye": "fa-crosshairs",
+    "visualization": "fa-street-view",
+};
+
+
+var exampleConfig = {
+    "imageryLayer": {
+        name: "基础底图",
+        content: {
+            "imageryLayer-online": {
+                name: "在线地图",
+                content: [ 
+                    {
+                        name: "天地图",
+                        version: "1.1.0",
+                        thumbnail: "11_online_tdt.jpg",
+                        fileName: "11_online_tdt"
+                    },
+                    {
+                        name: "谷歌",
+                        version: "1.1.0",
+                        thumbnail: "11_online_google.jpg",
+                        fileName: "11_online_google"
+                    },
+                    {
+                        name: "高德",
+                        version: "1.1.0",
+                        thumbnail: "11_online_gaode.jpg",
+                        fileName: "11_online_gaode"
+                    },
+                    {
+                        name: "百度",
+                        version: "1.7.6",
+                        thumbnail: "11_online_baidu.jpg",
+                        fileName: "11_online_baidu"
+                    },
+                   
+                ]
+            },
+            "imageryLayer-geshi": {
+                name: "自定义格式地图(可离线)",
+                content: [ 
+                    {
+                        name: "xyz金字塔瓦片",
+                        version: "1.1.0",
+                        thumbnail: "12_layer_xyz.jpg",
+                        fileName: "12_layer_xyz"
+                    }, 
+                      {
+                        name: "OGC WMS服务",
+                        version: "1.1.0",
+                        thumbnail: "12_layer_wms.jpg",
+                        fileName: "12_layer_wms"
+                    }, {
+                        name: "OGC WMTS服务",
+                        version: "1.1.0",
+                        thumbnail: "12_layer_wmts.jpg",
+                        fileName: "12_layer_wmts"
+                    }, 
+                    {
+                        name: "EPSG4490坐标系",
+                        version: "1.9.2",
+                        thumbnail: "12_layer_4490.jpg",
+                        fileName: "12_layer_4490"
+                    },
+                    
+                ]
+            },
+           
+            "imageryLayer-config": {
+                name: "通过SDK配置(推荐)",
+                content: [
+                    {
+                        name: "json配置文件",
+                        version: "1.0.0",
+                        thumbnail: "13_for_config.jpg",
+                        fileName: "13_for_config"
+                    }, {
+                        name: "json文件url动态传参",
+                        version: "1.0.0",
+                        thumbnail: "13_for_config_url.jpg",
+                        fileName: "13_for_config_url",
+                        params: "config=config.json"
+                    }, {
+                        name: "json格式数据",
+                        version: "1.0.0",
+                        thumbnail: "13_for_config_data.jpg",
+                        fileName: "13_for_config_data"
+                    }, {
+                        name: "与第3方SDK结合使用",
+                        version: "2.1.0",
+                        thumbnail: "13_otherSDK.jpg",
+                        fileName: "13_otherSDK"
+                    }
+
+                    
+                ]
+            },
+        }
+    },
+
+    "feature": {
+        name: "矢量数据",
+        content: {
+           
+            "feature-point": {
+                name: "点状对象",
+                content: [
+                   
+                    {
+                        name: "动态效果点",
+                        version: "1.7.6",
+                        thumbnail: "22_point_animation.gif",
+                        fileName: "22_point_animation"
+                    },
+                   
+                ]
+            },
+            "feature-polyline": {
+                name: "线状对象",
+                content: [ 
+                    {
+                        name: "动态效果线",
+                        version: "1.6.9",
+                        thumbnail: "23_animationLine3.gif",
+                        fileName: "23_animationLine3"
+                    },
+                    
+                    {
+                        name: "动态立体墙",
+                        version: "1.6.9",
+                        thumbnail: "23_wall_animation.gif",
+                        fileName: "23_wall_animation"
+                    }, 
+                ]
+            },
+            
+            "feature-other": {
+                name: "其他立体状",
+                content: [
+                   
+                    {
+                        name: "卫星视锥体",
+                        version: "1.8.9",
+                        thumbnail: "27_space_satellite.jpg",
+                        fileName: "27_space_satellite",
+                        plugins: "space",
+                    }
+                ]
+            },
+            "feature-geshi": {
+                name: "通用格式数据支持",
+                content: [
+                    {
+                        name: "GeoJson (地块)",
+                        version: "2.0.5",
+                        thumbnail: "28_geojson.jpg",
+                        fileName: "28_geojson"
+                    },
+                    {
+                        name: "GeoJson (行政区划)",
+                        version: "1.2.0",
+                        thumbnail: "28_geojson2.jpg",
+                        fileName: "28_geojson2"
+                    },
+                    
+                ]
+            },
+         
+            "feature-edit": {
+                name: "编辑及控制",
+                content: [
+                    {
+                        name: "标绘 （绘制文字、点、线、面、立体等）",
+                        version: "1.7.6",
+                        thumbnail: "29_draw.jpg",
+                        fileName: "29_draw"
+                    },
+                    {
+                        name: "军事标绘",
+                        version: "1.9.1",
+                        thumbnail: "29_draw_pro.jpg",
+                        fileName: "29_draw_pro"
+                    },
+                   
+                ]
+            }
+        }
+    },
+    "gltf": {
+        name: "小模型",
+        content: { 
+            "gltf-edit": {
+                name: "数据编辑控制",
+                content: [
+                    {
+                        name: "标绘模型",
+                        version: "1.7.6",
+                        thumbnail: "33_draw_gltf.jpg",
+                        fileName: "33_draw_gltf",
+                    },
+                     
+                ]
+            },
+        }
+    },
+     
+    "scene": {
+        name: "场景",
+        content: {
+            "scene-control": {
+                name: "控件",
+                content: [
+                    {
+                        name: "Popup鼠标单击信息窗",
+                        version: "1.3.0",
+                        thumbnail: "41_popup.jpg",
+                        fileName: "41_popup"
+                    },
+                    {
+                        name: "Tooltip鼠标移入信息",
+                        version: "1.3.0",
+                        thumbnail: "41_tooltip.jpg",
+                        fileName: "41_tooltip"
+                    },
+                    {
+                        name: "右键菜单",
+                        version: "1.8.9",
+                        thumbnail: "41_contextmenu.jpg",
+                        fileName: "41_contextmenu"
+                    },
+                    
+                ]
+            },
+     
+            "scene-camera": {
+                name: "相机及视角",
+                content: [
+                   
+                    {
+                        name: "空中漫游",
+                        version: "1.8.9",
+                        thumbnail: "43_roaming_air.gif",
+                        fileName: "43_roaming_air"
+                    },
+                    {
+                        name: "行人漫游",
+                        version: "2.0.2",
+                        thumbnail: "43_roaming_man.jpg",
+                        fileName: "43_roaming_man"
+                    },
+                    {
+                        name: "贴地表表面漫游",
+                        version: "1.8.9",
+                        thumbnail: "43_roaming_ground.gif",
+                        fileName: "43_roaming_ground "
+                    },
+                    {
+                        name: "贴模型表面漫游",
+                        version: "2.0.2",
+                        thumbnail: "43_roaming_ground2.gif",
+                        fileName: "43_roaming_ground2 "
+                    },
+
+                    
+                    {
+                        name: "飞行路线(复杂)",
+                        version: "1.6.9",
+                        thumbnail: "43_fly_timeinfo.jpg",
+                        fileName: "43_fly_timeinfo"
+                    },
+                   
+                ]
+            },
+
+            "scene-jiaohu": {
+                name: "交互性",
+                content: [
+                    {
+                        name: "鼠标事件简化处理",
+                        version: "1.7.6",
+                        thumbnail: "44_event.jpg",
+                        fileName: "44_event"
+                    },
+                    
+                ]
+            },
+          
+        }
+    },
+    "analysi": {
+        name: "分析",
+        content: {
+            "analysi": {
+                name: "空间分析",
+                content: [
+                    {
+                        name: "量算 (长度、面积、高度、角度)",
+                        version: "1.6.9",
+                        thumbnail: "61_measure.jpg",
+                        fileName: "61_measure"
+                    },
+                    
+                ]
+            },
+            
+           
+        }
+    },
+    "hangye": {
+        name: "行业应用",
+        content: {
+             
+            "hangye-sapce": {
+                name: "航天卫星",
+                content: [
+                   
+                    {
+                        name: "多卫星实时展示",
+                        version: "2.0.2",
+                        thumbnail: "73_satellite_list.jpg",
+                        fileName: "73_satellite_list",
+                        plugins: "space",
+                    },
+                    
+                ]
+            },
+         
+        }
+    },
+   
+ 
+
+};
+
+ 
